9b442a6eff3539435b408feacc0dced62b54ac5f
José Maria Araújo <josemariaaraujo@gmail.com>
2013-04-26 17:56:51 +0100

    First fully functional release

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,13 @@
             this.comboBoxCOMPorts = new System.Windows.Forms.ComboBox();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.WriteData = new System.Windows.Forms.Button();
+            this.ReadData = new System.Windows.Forms.Button();
+            this.ReadOBIS = new System.Windows.Forms.TextBox();
+            this.WriteOBIS = new System.Windows.Forms.TextBox();
+            this.ProgramMode = new System.Windows.Forms.CheckBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonStart
@@ -71,7 +78,6 @@
             // 
             this.serialPort1.BaudRate = 300;
             this.serialPort1.DataBits = 7;
-            this.serialPort1.DtrEnable = true;
             this.serialPort1.Parity = System.IO.Ports.Parity.Even;
             this.serialPort1.RtsEnable = true;
             this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
@@ -79,6 +85,9 @@
             // richTextBox1
             // 
             this.richTextBox1.AcceptsTab = true;
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.richTextBox1.DetectUrls = false;
             this.richTextBox1.Location = new System.Drawing.Point(13, 42);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
@@ -86,19 +95,94 @@
             this.richTextBox1.Size = new System.Drawing.Size(259, 208);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
-            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            // 
+            // WriteData
+            // 
+            this.WriteData.Enabled = false;
+            this.WriteData.Location = new System.Drawing.Point(278, 123);
+            this.WriteData.Name = "WriteData";
+            this.WriteData.Size = new System.Drawing.Size(114, 23);
+            this.WriteData.TabIndex = 0;
+            this.WriteData.Text = "Write";
+            this.WriteData.UseVisualStyleBackColor = true;
+            this.WriteData.Click += new System.EventHandler(this.WriteData_Click);
+            // 
+            // ReadData
+            // 
+            this.ReadData.Enabled = false;
+            this.ReadData.Location = new System.Drawing.Point(278, 68);
+            this.ReadData.Name = "ReadData";
+            this.ReadData.Size = new System.Drawing.Size(114, 23);
+            this.ReadData.TabIndex = 0;
+            this.ReadData.Text = "Read";
+            this.ReadData.UseVisualStyleBackColor = true;
+            this.ReadData.Click += new System.EventHandler(this.ReadData_Click);
+            // 
+            // ReadOBIS
+            // 
+            this.ReadOBIS.Location = new System.Drawing.Point(278, 97);
+            this.ReadOBIS.Name = "ReadOBIS";
+            this.ReadOBIS.Size = new System.Drawing.Size(114, 20);
+            this.ReadOBIS.TabIndex = 4;
+            this.ReadOBIS.Text = "0.9.1()";
+            // 
+            // WriteOBIS
+            // 
+            this.WriteOBIS.Location = new System.Drawing.Point(278, 152);
+            this.WriteOBIS.Name = "WriteOBIS";
+            this.WriteOBIS.Size = new System.Drawing.Size(114, 20);
+            this.WriteOBIS.TabIndex = 4;
+            this.WriteOBIS.Text = "0.9.1()";
+            // 
+            // ProgramMode
+            // 
+            this.ProgramMode.AutoSize = true;
+            this.ProgramMode.Checked = true;
+            this.ProgramMode.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ProgramMode.Location = new System.Drawing.Point(278, 16);
+            this.ProgramMode.Name = "ProgramMode";
+            this.ProgramMode.Size = new System.Drawing.Size(79, 17);
+            this.ProgramMode.TabIndex = 5;
+            this.ProgramMode.Text = "Programing";
+            this.ProgramMode.UseVisualStyleBackColor = true;
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(334, 42);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(58, 20);
+            this.Password.TabIndex = 6;
+            this.Password.Text = "00000000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Password:";
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(404, 267);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.ProgramMode);
+            this.Controls.Add(this.WriteOBIS);
+            this.Controls.Add(this.ReadOBIS);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.comboBoxCOMPorts);
             this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.ReadData);
+            this.Controls.Add(this.WriteData);
             this.Controls.Add(this.buttonStart);
             this.Name = "Form1";
+            this.Text = "62056-21";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -109,6 +193,13 @@
         private System.Windows.Forms.ComboBox comboBoxCOMPorts;
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button WriteData;
+        private System.Windows.Forms.Button ReadData;
+        private System.Windows.Forms.TextBox ReadOBIS;
+        private System.Windows.Forms.TextBox WriteOBIS;
+        private System.Windows.Forms.CheckBox ProgramMode;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label1;
 
 
     }
--- Form1.cs
+++ Form1.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
+//using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
 
@@ -14,24 +14,34 @@ namespace IEC62056_21
     public partial class Form1 : Form
     {
         string RxString;
-
+        string RxBuffer;
+        
         public Form1()
         {
             InitializeComponent();
         }
-
+       
         private void buttonStart_Click(object sender, EventArgs e)
         {
             if (comboBoxCOMPorts.SelectedIndex < 0)
                 return;
-            
+
+            RxBuffer = "";
+            serialPort1.BaudRate = 300;
             serialPort1.Open();
             if (serialPort1.IsOpen)
             {
                 buttonStart.Enabled = false;
                 buttonStop.Enabled = true;
+                ProgramMode.Enabled = false;
+                if (ProgramMode.Checked)
+                {
+                    ReadData.Enabled = true;
+                    WriteData.Enabled = true;
+                }
                 //richTextBox1.ReadOnly = false;
                 comboBoxCOMPorts.Enabled = false;
+                serialPort1.Write("/?!\r\n");
             }
          }
 
@@ -39,9 +49,18 @@ namespace IEC62056_21
         {
             if (serialPort1.IsOpen)
             {
+                if (ProgramMode.Checked)
+                {
+                    serialPort1.Write("\x01" + "B0" + "\x03" + "q");
+                    System.Threading.Thread.Sleep(1000);
+                }
+                
                 serialPort1.Close();
                 buttonStart.Enabled = true;
                 buttonStop.Enabled = false;
+                ReadData.Enabled = false;
+                WriteData.Enabled = false;
+                ProgramMode.Enabled = true;
                // richTextBox1.ReadOnly = true;
                 comboBoxCOMPorts.Enabled = true;
             }
@@ -50,7 +69,41 @@ namespace IEC62056_21
         private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
         {
             RxString = serialPort1.ReadExisting();
+            RxBuffer = RxBuffer + RxString;
             this.Invoke(new EventHandler(DisplayText));
+            ProcessData();
+
+        }
+        private void ProcessData()
+        {
+            foreach (char c in RxString)
+                if ( c == 0x0A )
+                {
+                    if (String.Compare("/ISK5", 0, RxBuffer, 0, 5) == 0)
+                    {
+                        if ( ProgramMode.Checked )
+                            serialPort1.Write("\x06" + "051\r\n");
+                        else
+                            serialPort1.Write("\x06" + "050\r\n");
+                        System.Threading.Thread.Sleep(250);
+                        serialPort1.BaudRate = 9600;
+                        serialPort1.DiscardInBuffer();
+                    }
+
+                    RxBuffer = "";
+                }
+                else if (c == 0x03 && ProgramMode.Checked)
+                {
+                    System.Threading.Thread.Sleep(250);
+                    if (String.Compare("\x01" + "P0", 0, RxBuffer, 0, 3) == 0)
+                    {
+                        serialPort1.Write("\x01" + ReturnBCC("P1" + "\x02" + "(" + Password.Text + ")" + "\x03"));
+                    }
+                    RxBuffer = "";
+                }
+
+
+
         }
 
         private void DisplayText(object sender, EventArgs e)
@@ -63,6 +116,7 @@ namespace IEC62056_21
                     richTextBox1.SelectionColor = Color.Red;
                     richTextBox1.AppendText("<"+((int)c).ToString()+">");
                     richTextBox1.SelectionColor = richTextBox1.ForeColor;
+                    if (c=='\x0a')richTextBox1.AppendText(Convert.ToString(c));
                 } else
                     richTextBox1.AppendText(Convert.ToString(c));
         }
@@ -88,7 +142,7 @@ namespace IEC62056_21
         {
             serialPort1.PortName = comboBoxCOMPorts.Text;
         }
-
+//DISABLED!!!
         private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
           // If the port is closed, don't try to send a character.
@@ -109,6 +163,32 @@ namespace IEC62056_21
             // display locally. If you want it to display, omit the next line.
             e.Handled = true;
         }
-     
+
+        private void ReadTime_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("\x01" +ReturnBCC("R1" + "\x02" + "0.9.1()" + "\x03"));
+        }
+
+        private string ReturnBCC(string input)
+        {
+           char bcc='\x0';
+           foreach (char c in input)
+               bcc ^= c;
+           return input + bcc;
+        }
+
+        private void ReadData_Click(object sender, EventArgs e)
+        {
+             serialPort1.Write("\x01" + ReturnBCC("R1" + "\x02" + ReadOBIS.Text + "\x03"));
+            //MessageBox.Show("#"+ReadOBIS.Text+"#");
+
+        }
+
+        private void WriteData_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("\x01" + ReturnBCC("W1" + "\x02" + WriteOBIS.Text + "\x03"));
+        }
+
+
     }
 }
